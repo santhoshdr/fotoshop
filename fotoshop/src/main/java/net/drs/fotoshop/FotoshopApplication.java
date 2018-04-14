a915bc0c6d1692baca8539b0d5d4ccdcc60f1915
srajanna <santhosh.rajanna@amadeus.com>
2018-04-14 10:13:03 +0530

    My First commit

---
+++ fotoshop/src/main/java/net/drs/fotoshop/FotoshopApplication.java
@@ -0,0 +1,12 @@
+package net.drs.fotoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoshopApplication.class, args);
+	}
+}
