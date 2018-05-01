abf5b9a491bc2b51f93c2ee3e6c8c3f70fd1d0dd
srajanna <santhosh.rajanna@amadeus.com>
2018-05-01 09:15:26 +0530

    sample checkin

--- fotoshop/src/test/java/net/drs/fotoshop/FotoshopApplicationTests.java
+++ fotoshop/src/test/java/net/drs/fotoshop/FotoshopApplicationTests.java
@@ -5,12 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FotoshopApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() {System.out.println("Test cases....");
 	}
 
 }
